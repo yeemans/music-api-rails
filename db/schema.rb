486aab6aceef0bdfaa3a7116770a203430770fc0
arthur <arthur.roth.07@gmail.com>
2023-01-20 14:15:30 -0800

    tired of this bullshit

--- db/schema.rb
+++ db/schema.rb
@@ -7,7 +7,7 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-#
+#it 
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 0) do
