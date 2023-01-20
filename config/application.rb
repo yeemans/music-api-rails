9292adccc376637e2aaacd23eec3634dc715cbba
arthur <arthur.roth.07@gmail.com>
2023-01-20 14:20:57 -0800

    ehakdfhaksfhadsjf

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module MusicApi2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.hosts << "music-api-rails-production.up.railway.app"
+    config.hosts << "https://music-api-rails-production.up.railway.app"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
