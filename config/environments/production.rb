8cd9e7b816851d62779d52b79d800eb911436232
arthur <arthur.roth.07@gmail.com>
2023-01-20 16:08:37 -0800

    added cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "music-api-rails-production.up.railway.app"
-  config.hosts << "https://music-api-rails-production.up.railway.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
